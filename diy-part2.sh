b77d9a4ff8ad907ddaa74b391ab7843c711dbe8f
Frank Zhang <9338143+xe5700@users.noreply.github.com>
2021-02-16 22:02:23 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,4 +15,4 @@
 wget https://raw.githubusercontent.com/openwrt/packages/master/net/ddns-scripts/files/usr/lib/ddns/update_cloudflare_com_v4.sh -O feeds/packages/net/ddns-scripts/files/update_cloudflare_com_v4.sh
 
 #./scripts/feeds install luci -a
-git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
+#git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
